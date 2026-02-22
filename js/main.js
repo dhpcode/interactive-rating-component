ef170bdd42e3d9cc74ee3c32912150e98a3a487f
dhpcode <davrsirius@gmail.com>
2026-02-22 11:32:20 +0500

    Select rating score

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 const scores = document.querySelectorAll('.rating__btn');
-console.log(scores)
+const selectedRating = document.querySelector('.selected-rating');
+
+console.log(selectedRating);
+
 
 scores.forEach(score => {
   score.addEventListener('click', () => {
     scores.forEach(score => score.classList.remove('btn-active'));
-    
+
     score.classList.add('btn-active');
   })
 
