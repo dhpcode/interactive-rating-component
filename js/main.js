4f65a57b587c305b3726d95a3f02d396ab6188ef
dhpcode <davrsirius@gmail.com>
2026-02-22 21:41:39 +0500

    Remove unnecessary elements and add some extra codes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 const scores = document.querySelectorAll('.rating__btn');
 const selectedRating = document.querySelector('.selected-rating');
+const submitBtn = document.querySelector('.rating__submit');
 
-console.log(selectedRating);
+
+//body
+const bodyEl = document.querySelector('body');
+
+// rating div
+const ratingEl = document.querySelector('.rating');
+
+
+//thanks
+const thanksEl = document.querySelector('.thanks');
 
 
 scores.forEach(score => {
@@ -9,6 +19,29 @@ scores.forEach(score => {
     scores.forEach(score => score.classList.remove('btn-active'));
 
     score.classList.add('btn-active');
+
+    const number = score.textContent;
+
+    selectedRating.textContent = number;
   })
 
 })
+
+submitBtn.addEventListener('click', () => {
+  ratingEl.classList.add('not-visible');
+
+  thanksEl.classList.remove('not-visible');
+})
+
+
+thanksEl.addEventListener('click', () => {
+  ratingEl.classList.remove('not-visible');
+
+  thanksEl.classList.add('not-visible');
+})
+
+// bodyEl.addEventListener('click', () => {
+//   ratingEl.classList.remove('not-visible');
+
+//   thanksEl.classList.add('not-visible');
+// })
